2030a509f55296b793cce9ca3267394a189b26b0
mrsteveson <patrick.steveson727@gmail.com>
2019-04-26 10:34:34 -0500

    projects-router fully functional

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,20 +4,111 @@ const db = require('../data/helpers/projectModel.js');
 const router = express.Router()
 
 // CRUD Operations
-// Get. **Postman Tested: **
+// Get. **Postman Tested: working**
+router.get('/', (req, res) => {
+    db
+    .get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The projects could not be retrieved."})
+    })
+})
 
+// Get by ID. **Postman Tested: working**
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
 
-// Get by ID. **Postman Tested: **
+    db
+    .get(id)
+    .then(project => {
+        if(project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ message: "The project with the specified ID does not exist."})
+        }  
+    })
+    .catch(err => {
+        res.status(500).json({ message: "An error occurred retrieving the project with the specified ID."})
+    })
+})
 
+// Post. **Postman Tested: working**
+router.post('/', (req, res) => {
+    const { name, description } = req.body;
 
-// Post. **Postman Tested: **
+    if(!name || !description) {
+        res.status(400).json({ message: "Please provide a name and description for this project."})
+    }
 
+    db
+    .insert({ name, description })
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "An error occurred creating the project."})
+    })
+})
 
-// Delete. **Postman Tested: **
+// Delete. **Postman Tested: working**
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
 
+    db
+    .remove(id)
+    .then(project => {
+        if(project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ message: "The project with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "An error occurred deleting the project."})
+    })
+})
 
-// Put/Update. **Postman Tested: **
+// Put/Update. **Postman Tested: working**
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, description } = req.body;
 
+    if(!name || !description) {
+        res.status(400).json({ message: "Please provide a name and description for this project."})
+    }
 
+    db
+    .update(id, { name, description })
+    .then(project => {
+        if(project) {
+            res.status(200).json(project)
+        } else {
+            res.status(404).json({ message: "The project with the specified ID does not exist."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "An error occurred updating the project."})
+    })
+})
+
+// Get all of a specified projects actions. **Postman Tested: working**
+router.get('/actions/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+    .getProjectActions(id)
+    .then(projectActions => {
+        if(projectActions) {
+            res.status(200).json(projectActions)
+        } else {
+            res.status(404).json({ message: "This project currently has no actions."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "An error occurred retrieving the project's actions."})
+    })
+})
 
 module.exports = router;
